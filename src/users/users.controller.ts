38fbf7390e583021a0719b037d1eac542d287c1e
gyedhu <gyedhumohanan@gmail.com>
2023-02-25 16:47:33 +0530

    fix: updated created-user to create and added OnlyAdminGuard, created new end-point '/profile'

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,8 +4,10 @@ import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { User } from "src/@common/decorators/user.decorator";
 import { UserInfoDto } from "src/@common/dtos/userInfo.dto";
 import { JwtAuthGuard } from "src/@common/guards/jwt-auth.guard";
+import { OnlyAdminGuard } from "src/@common/guards/only-admin.guards";
 import { ListUserResponseDto } from "src/topics/dtos/listTopics.dto";
 import { CreateUserBodyDto, CreateUserResponseDto } from "./dto/createUser.dto";
+import { ProfileResponseDto } from "./dto/profile.dto";
 import { UsersService } from "./users.service";
 
 @ApiTags("Users")
@@ -14,10 +16,17 @@ import { UsersService } from "./users.service";
 export class UsersController {
   constructor(private readonly userService: UsersService) { }
 
+  @UseGuards(OnlyAdminGuard)
   @ApiResponse({ status: 201, type: [CreateUserResponseDto] })
-  @Post("/create-user")
+  @Post("/create")
   async createUser(@Body() body: CreateUserBodyDto, @User() userInfo: UserInfoDto): Promise<CreateUserResponseDto> {
     return this.userService.createUser(body, userInfo);
+  }
+
+  @ApiResponse({ status: 200, type: [ProfileResponseDto] })
+  @Get("/profile")
+  async profile(@User() userInfo: UserInfoDto): Promise<ProfileResponseDto> {
+    return this.userService.profile(userInfo);
   }
 
   @ApiResponse({ status: 200, type: [ListUserResponseDto] })
